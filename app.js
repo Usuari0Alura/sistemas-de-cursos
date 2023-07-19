c7fd5320387740f3728b18861067f082819f058e
Eduardoplay <eduardo.esser@ganex.com.br>
2023-07-19 15:29:03 -0300

    Alterando Contato para Staging

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 // mais um consolo log de teste
 console.log("O usuário dez esse commit sem voce ver na aula")
+confirm(A aplicação do sistema do CustomElementRegistry)
